ed31418eb39114725c0de1b16b4c80d1690a9015
Jean-Marc AUBERT <jmaubert1333@gmail.com>
2018-03-06 11:16:39 +0100

    Class Hero

---
+++ src/Hero.java
@@ -0,0 +1,41 @@
+
+public class Hero {
+
+	public String name;
+	public int life;
+	public int maxLife;
+	public int stamina;
+	public int maxStamina;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLife() {
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public int getMaxLife() {
+		return maxLife;
+	}
+	public void setMaxLife(int maxLife) {
+		this.maxLife = maxLife;
+	}
+	public int getStamina() {
+		return stamina;
+	}
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+	public int getMaxStamina() {
+		return maxStamina;
+	}
+	public void setMaxStamina(int maxStamina) {
+		this.maxStamina = maxStamina;
+	}
+	
+	
+}
