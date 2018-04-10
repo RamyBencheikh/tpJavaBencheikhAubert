d04be0a3b752b94102e0fb148f4e4d72c8784aaa
Ramy BEN CHEIKH <ramy.bencheikh@ynov.com>
2018-04-10 11:52:16 +0200

    Mise en commun && Creation classe Character

--- src/Hero.java
+++ src/Hero.java
@@ -1,41 +1,50 @@
 
 public class Hero {
 
-	public String name;
-	public int life;
-	public int maxLife;
-	public int stamina;
-	public int maxStamina;
-	public String getName() {
+	private String name;
+	private int life;
+	private int maxLife;
+	private int stamina;
+	private int maxStamina;
+
+	private String getName() {
+
 		return name;
 	}
-	public void setName(String name) {
+	private void setName(String name) {
+
 		this.name = name;
 	}
-	public int getLife() {
+	private int getLife() {
+
 		return life;
 	}
-	public void setLife(int life) {
+	private void setLife(int life) {
+
 		this.life = life;
 	}
-	public int getMaxLife() {
+	private int getMaxLife() {
+
 		return maxLife;
 	}
-	public void setMaxLife(int maxLife) {
+	private void setMaxLife(int maxLife) {
+
 		this.maxLife = maxLife;
 	}
-	public int getStamina() {
+	private int getStamina() {
+
 		return stamina;
 	}
-	public void setStamina(int stamina) {
+	private void setStamina(int stamina) {
+
 		this.stamina = stamina;
 	}
 	public int getMaxStamina() {
+
 		return maxStamina;
 	}
 	public void setMaxStamina(int maxStamina) {
+
 		this.maxStamina = maxStamina;
 	}
-	
-	
 }
